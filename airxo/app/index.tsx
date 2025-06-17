7da26294e24c1680ffcc08a3b41f0efb0160cb66
Crinsay <martyna.loboda@outlook.com>
2025-06-17 17:10:47 +0200

    feature: added app name and info button

--- airxo/app/index.tsx
+++ airxo/app/index.tsx
@@ -1,13 +1,20 @@
 import { Link } from "expo-router";
-import { ScrollView, Image, View } from "react-native";
+import { ScrollView, Image, View, Text } from "react-native";
 import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
 
 export default function Index() {
   return (
     <View className="flex-1 bg-background">
       <Image source={images.bg} className="absolute w-full h-full" resizeMode="cover"/>
-      <View className="flex-1 mt-20 px-6">
+      <View className="flex-row items-center justify-between px-6 pt-10 mt-6">
+        <Text className="text-foreground text-3xl font-bold">Airxo - Air Quality</Text>
+        <Link href="/info" className="text-foreground text-lg font-bold">
+          <Image source={icons.questionMark} className="w-7 h-7" />
+        </Link>
+      </View>
+      <View className="flex-1 mt-6 px-6">
         <SearchBar
           placeholder="Search city"
         />
--- airxo/constants/icons.ts
+++ airxo/constants/icons.ts
@@ -1,5 +1,7 @@
 import search from "@/assets/icons/search.png";
+import questionMark from "@/assets/icons/question.png";
 
 export const icons = {
-  search
+  search,
+  questionMark
 };
